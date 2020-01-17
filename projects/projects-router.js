6866a05ccd09053c89128c8d24654b427a678966
Robert Gordon <Komeuppance@gmail.com>
2020-01-17 08:37:33 -0800

    Getting data success

---
+++ projects/projects-router.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const Projects = require('../data/helpers/projectModel.js');
+
+const mdlware = require('../middleware/index.js');
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => next(error));
+});
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the project',
+      });
+    });
+  });
+
+router.post('/', (req, res, next) => {
+  Projects.insert(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(error => next(error));
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(express.json());
 server.use(logger);
 
 server.use('/api/projects', projectsRouter);
-server.use('/api/actions', actionsRouter);
+// server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>Hello universe.</h2>`);
