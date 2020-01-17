39e8faafd79fa52737cdfa5f0e40a7408438dcc9
Robert Gordon <Komeuppance@gmail.com>
2020-01-17 09:13:40 -0800

    Additional endpoint projects/:id/actions success

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -34,6 +34,24 @@ router.get('/:id', (req, res) => {
     });
   });
 
+router.get('/:id/actions', (req, res) => {
+    Projects.getProjectActions(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the project',
+      });
+    });
+  });
+
 router.post('/', mdlware.validate('name'), mdlware.validate('description'), (req, res, next) => {
   Projects.insert(req.body)
     .then(project => {
@@ -78,5 +96,5 @@ router.put('/:id', (req, res) => {
       });
     });
   });
-  
+
 module.exports = router;
